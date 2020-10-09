e69468aeb9a9fbcffb9a51819d85d880b8c191cb
庄曦 <2939898974@qq.com>
2020-10-09 15:07:06 +0800

    数据适配器已完成

---
+++ app/src/main/java/com/example/recyclerviewdemo/FruitAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.recyclerviewdemo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class FruitAdapter (val fruitList: List<Fruit>):RecyclerView.Adapter<FruitAdapter.ViewHolder>(){
+    inner class ViewHolder (view: View):RecyclerView.ViewHolder(view){
+        val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
+        val fruitName: TextView = view.findViewById(R.id.fruitName)
+        val fruitTime:TextView = view.findViewById(R.id.fruitTime)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.fruit_item,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = fruitList.count()
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+       val fruit = fruitList[position]
+        holder.fruitImage.setImageResource(fruit.imageID)
+        holder.fruitName.text = fruit.name
+        holder.fruitTime.text = getTime()
+    }
+
+    fun getTime():String{
+        val data = Date()
+        val dateFormat = SimpleDateFormat("HH:mm:ss", Locale.CHINA)
+        return dateFormat.format(data)
+    }
+
+}
